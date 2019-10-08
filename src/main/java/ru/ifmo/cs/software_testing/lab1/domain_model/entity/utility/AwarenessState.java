5b6fdc15194df034ac84ca42f2e362dd3023739e
macbook <kir-esakov2011@mail.ru>
2019-10-09 01:41:10 +0300

    without testing

--- src/main/java/ru/ifmo/cs/software_testing/lab1/domain_model/entity/utility/AwarenessState.java
+++
@@ -1,5 +0,0 @@
-package ru.ifmo.cs.software_testing.lab1.domain_model.entity.utility;
-
-public enum  AwarenessState {
-	AWARE, TRYING_TO_AWARE, NOT_AWARE;
-}
--- src/main/java/ru/ifmo/cs/software_testing/lab1/domain_model/entity/utility/RelationType.java
+++
@@ -1,5 +0,0 @@
-package ru.ifmo.cs.software_testing.lab1.domain_model.entity.utility;
-
-public enum RelationType {
-	IS, KNOW_ABOUT
-}
